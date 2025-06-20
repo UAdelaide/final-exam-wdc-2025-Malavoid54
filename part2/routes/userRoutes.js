a76091007949d6d0d6dcbc6cf488a83610ef6c62
Malavoid54 <fergus.weinert@gmail.com>
2025-06-20 05:13:55 +0000

    [automatic commit] 2025-06-20 05:13:55 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -45,7 +45,9 @@ router.get('/mydogs', async (req, res) => {
 
   try {
     const [rows] = await db.query(
-      'SELECT dog_id, name FROM Dogs WHERE owner_id'
+      'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
+      [req.session.user.user_id]
+      
     )
   }
 })
