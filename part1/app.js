940607bbdf8981c5b9fc743645d22fde56bfa581
Malavoid54 <fergus.weinert@gmail.com>
2025-06-20 01:42:46 +0000

    [automatic commit] 2025-06-20 01:42:45 UTC

--- part1/app.js
+++ part1/app.js
@@ -31,7 +31,9 @@ async function seedData() {
             ((SELECT DogID FROM Dogs WHERE DogName = 'Max'), '2025-06-10 08:00:00', 60, 'RiverBank Arena', 'open'),
             ((SELECT DogID FROM Dogs WHERE DogName = 'Max'), '2025-06-10 08:00:00', 20, 'Downtown', 'cancelled')
             `);
-        console.log
+        console.log("Full seed data inserted.");
+    } catch (err) {
+        
     }
 
 }
