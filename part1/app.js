cc171ff7c4179456264fc5b841e81a53001680c5
Malavoid54 <fergus.weinert@gmail.com>
2025-06-20 01:29:36 +0000

    [automatic commit] 2025-06-20 01:29:36 UTC

--- part1/app.js
+++ part1/app.js
@@ -14,7 +14,7 @@ async function seedData() {
              ('alice123', 'alice@example.com', 'hashed123', 'owner'),
              ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
              ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-             ()
+             ('iluvd0gs', 'iluvd0gs123@example.com)
         `);
     }
 
