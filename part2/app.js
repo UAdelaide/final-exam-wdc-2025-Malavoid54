d81072edd1a95f53db8dad9e3f9db7343ac8c926
Malavoid54 <fergus.weinert@gmail.com>
2025-06-20 04:18:48 +0000

    [automatic commit] 2025-06-20 04:18:48 UTC

--- part2/app.js
+++ part2/app.js
@@ -57,8 +57,9 @@ app.post('/login', async (req, res) => {
 app.post('./logout', (req, res) => {
     req.session.destroy(err => {
         if (err) {
-            return res.status(500).json({ error: 'Logout failed' })
+            return res.status(500).json({ error: 'Logout failed' });
         }
+        res.clearCookie('connect')
     })
 })
 
