5eb9352523ab2b149afe31623306dcf00d11792b
Malavoid54 <fergus.weinert@gmail.com>
2025-06-20 01:27:11 +0000

    [automatic commit] 2025-06-20 01:27:11 UTC

--- part1/app.js
+++ part1/app.js
@@ -11,7 +11,8 @@ app.use('./api', apiRoutes);
 async function seedData() {
     try {
         await db.execute('INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
-             ('alice123', 'alice@example.com', 'hashed123', 'owner')');
+             ('alice123', 'alice@example.com', 'hashed123', 'owner')
+        ');
     }
 
 }
